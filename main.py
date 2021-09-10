15e86feafa37e83a001eacdd0de0ed499922b8fc
ArmanZarei <armanzarei1378@gmail.com>
2021-09-10 13:12:20 -0700

    Changes on main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from models.pointnet import PointNet
-from utils import get_model_output, knn
+from utils import get_model_output, knn, get_model
 from torch.utils import data
 from dataset import LettucePointCloudDataset
 import torch
@@ -7,39 +7,42 @@ from models.pointnet2 import PointNet2
 import numpy as np
 import multiprocessing as mp
 import time
+import argparse
+from tqdm import tqdm
 
 
-n_samples = 1500
-K = 5
+parser = argparse.ArgumentParser()
+parser.add_argument('--K', type=int, default=51)
+parser.add_argument('--n_samples', type=str, default=1500)
+parser.add_argument('--model', type=str, default='pointnet2', choices=['pointnet', 'pointnet2', 'randlanet'])
+args = parser.parse_args()
 
 dataset = LettucePointCloudDataset(files_dir='./data')
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print(f'Device: {device}\n{"-"*30}')
+print(f'Device: {device}\n{"-"*15}')
 
-model = PointNet2(2).to(device)
-model.load_state_dict(torch.load('./pretrained_models/PointNet2.pth'))
+model = get_model(args.model, device)
 
-for (f_path, points) in dataset:
-    sampled_indices = np.random.choice(points.shape[0], n_samples)
+print("Segmenting PointClouds...")
+for (f_path, points) in tqdm(dataset):
+    sampled_indices = np.random.choice(points.shape[0], args.n_samples, replace=False)
     sampled_indices_lookup = set(sampled_indices)
     sampled_points = points[sampled_indices]
-    sampled_labels = get_model_output(model, sampled_points.float().unsqueeze(0).to(device)).argmax(1).squeeze().cpu()
+    sampled_labels = get_model_output(model, torch.from_numpy(sampled_points).float().unsqueeze(0).to(device)).argmax(1).squeeze().cpu().numpy()
 
-    labels = torch.zeros(points.shape[0], dtype=int)
+    labels = np.zeros(points.shape[0], dtype=int)
     labels[sampled_indices] = sampled_labels
 
+    p = mp.Pool(mp.cpu_count() - 2)
 
-    p = mp.Pool(2) # mp.cpu_count() - 2
     start_time = time.time()
-    print("START")
-    other_labels = p.starmap(knn, [(points[i], sampled_points, labels, K) for i in range(points.shape[0]) if i not in sampled_indices_lookup])
-    print('Time:', time.time()-start_time)
+    other_labels = p.starmap(knn, [(points[i], sampled_points, sampled_labels, args.K) for i in range(points.shape[0]) if i not in sampled_indices_lookup])
+    # print('KNN Log ===> Shape:', points.shape, '----', 'Time:', time.time()-start_time)
+
     others_indices_mask = np.ones(points.shape[0], dtype=bool)
     others_indices_mask[sampled_indices] = False
 
     labels[others_indices_mask] = other_labels
 
-    print(labels.shape)
     np.save(f_path.replace('.ply', '.npy'), labels)
-    exit()
