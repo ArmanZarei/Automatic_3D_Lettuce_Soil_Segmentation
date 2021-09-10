8215ab9aa1c157b49e7a8525c9f5b0960effff32
ArmanZarei <armanzarei1378@gmail.com>
2021-09-10 13:12:05 -0700

    Add get_model to utils

--- utils.py
+++ utils.py
@@ -37,3 +37,18 @@ def knn(point, sampled_points, sampled_labels, K, scored_knn=True):
         scores[sampled_labels[idx]] += 1/(distances[idx]+1e-10)
 
     return scores.argmax()
+
+def get_model(model_name, device):
+    if model_name == 'pointnet':
+        model = PointNet().to(device)
+        model.load_state_dict(torch.load('./pretrained_models/PointNet.pth', map_location=device))
+    elif model_name == 'pointnet2':
+        model = PointNet2(2).to(device)
+        model.load_state_dict(torch.load('./pretrained_models/PointNet2.pth', map_location=device))
+    elif model_name == 'randlanet':
+        model = RandLANet(d_in=3, num_classes=2, num_neighbors=16, decimation=4, device=device).to(device)
+        model.load_state_dict(torch.load('./pretrained_models/RandLANet.pth', map_location=device))
+    else:
+        raise Exception("invalid model.")
+    
+    return model
