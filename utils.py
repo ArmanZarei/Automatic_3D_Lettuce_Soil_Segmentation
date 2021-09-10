bf865eaae00375117195638dd3c3651e39d82716
ArmanZarei <armanzarei1378@gmail.com>
2021-09-10 10:46:36 -0700

    Add get_model_output_and_loss utility

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 import numpy as np
+from models.pointnet2 import PointNet2
+from models.pointnet import PointNet
+from models.randlanet import RandLANet
 
 
 def normalize_points(points):
@@ -6,3 +9,18 @@ def normalize_points(points):
     points /= np.linalg.norm(points, axis=1).max()
 
     return points
+
+def get_model_output_and_loss(model, input, labels, calculate_loss=True):
+    """
+    Returns the output of the model according to model type
+    Parameters:
+        model (Type[nn.Module]): Model
+        input (Tensor): input
+    """
+    if isinstance(model, PointNet):
+        outputs, _, _ = model(input)
+        return outputs
+    elif isinstance(model, (RandLANet, PointNet2)):
+        return model(input)
+    
+    raise Exception("Model should be of type PointNet or RandLANet or PointNet++ (PointNet2)")
